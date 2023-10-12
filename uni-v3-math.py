c47a7b0defac7fbc4136101890130324940903ac
Pawee Tantivasdakarn <paweetantivasdakarn@Pawees-MacBook-Pro-2.local>
2023-10-12 19:04:54 +0700

    feat: update stuffs

--- uni-v3-math.py
+++ uni-v3-math.py
@@ -12,5 +12,48 @@ def price_to_sqrtp(p):
     return int(math.sqrt(p) * q96)
 
 
+def liquidity0(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return (amount * (pa * pb) / q96) / (pb - pa)
+
+
+def liquidity1(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return amount * q96 / (pb - pa)
+
+
+def calc_amount0(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * q96 * (pb - pa) / pa / pb)
+
+
+def calc_amount1(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q96)
+
+
 print("Price 5000 to tick: ", price_to_tick(5000))
 print("Price 5000 to sqrt(P) in Q96 form: ", price_to_sqrtp(5000))
+
+sqrtp_low = price_to_sqrtp(4545)
+sqrtp_cur = price_to_sqrtp(5000)
+sqrtp_upp = price_to_sqrtp(5500)
+
+decimals = 10**18
+amount_eth = 1 * decimals
+amount_usdc = 5000 * decimals
+
+liq0 = liquidity0(amount_eth, sqrtp_cur, sqrtp_upp)
+liq1 = liquidity1(amount_usdc, sqrtp_cur, sqrtp_low)
+
+liq = int(min(liq0, liq1))
+print("Lower L is: ", liq)
+
+amount0 = calc_amount0(liq, sqrtp_upp, sqrtp_cur)
+amount1 = calc_amount1(liq, sqrtp_low, sqrtp_cur)
+
+print("Amount0, Amount1", amount0, amount1)
